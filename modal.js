0ee66028b1e84e4e4afb59e003af0b779a03e80f
Davi <104698942+DevDaviRV@users.noreply.github.com>
2022-07-12 03:55:32 -0300

    Add files via upload
    Site finalizado e atualizado

---
+++ modal.js
@@ -0,0 +1,6 @@
+function AbrirModal(){
+    document.getElementById('modaljs').style.top = '0';
+}
+function FecharModal() {
+    document.getElementById('modaljs').style.top = '-100%';
+}
---
+++ scrollreveal.js
@@ -0,0 +1,8 @@
+
+//API
+window.sr = ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.tabela-service', {delay:200});
+ScrollReveal().reveal('.painel-sobre', {delay:200});
+ScrollReveal().reveal('.painel-mapa', {delay:200});
+ScrollReveal().reveal('main', {delay:200});
